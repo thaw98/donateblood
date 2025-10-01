f56223c10715a3fd74d11d5db96ae42db9a58a7d
Pyae Phyo Thaw <ppthaw1998@gmail.com>
2025-10-01 21:29:28 +0630

    initial commit

---
+++ src/test/java/com/grppj/donateblood/DonatebloodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grppj.donateblood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonatebloodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
